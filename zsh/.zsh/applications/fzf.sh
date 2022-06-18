c10dee00a6d62fd6cc7a51b7bce46ca076ca592b
guppy0130 <guppy0130@yahoo.com>
2022-06-18 16:49:46 -0500

    have to pathmunge fzf location before searching

--- zsh/.zsh/applications/fzf.sh
+++ zsh/.zsh/applications/fzf.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 # vim:tw=2:sw=2
 
+pathmunge "$HOME/.fzf/bin"
+
 # if fzf isn't installed, install it (and don't generate ~/.fzf.{bash,zsh} to keep $HOME clean)
 [ -x "$(command -v fzf)" ] || (git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --no-key-bindings --no-completion --no-update-rc --bin)
-
-pathmunge "$HOME/.fzf/bin"
