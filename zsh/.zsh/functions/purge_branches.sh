8e6003aaca1c77ecbf759ffbda5fae89d4a0c0c2
guppy0130 <guppy0130@yahoo.com>
2023-11-13 12:43:07 -0600

    [misc] purge_branches update, tmux weather url-encode space

--- zsh/.zsh/functions/purge_branches.sh
+++ zsh/.zsh/functions/purge_branches.sh
@@ -3,7 +3,19 @@
 BRANCH_TMP_FILE="${TMPDIR:-/tmp/}merged-branches"
 
 purge_branches() {
-  BRANCH="${1:-main}"
-  git branch --merged "$BRANCH" | grep -v "$BRANCH" >"$BRANCH_TMP_FILE" &&
-    ${EDITOR:-nvim} "$BRANCH_TMP_FILE" && xargs git branch -d <"$BRANCH_TMP_FILE"
+  default_branch="$(basename "$(git symbolic-ref refs/remotes/origin/HEAD)")"
+  BRANCH="${1:-$default_branch}"
+
+  # ones that are already merged
+  git branch --merged "$BRANCH" | grep -v "$BRANCH" >"$BRANCH_TMP_FILE"
+  # ones that are gone
+  git branch --list --format "%(if:equals=[gone])%(upstream:track)%(then)%(refname:short)%(end)" | grep . >>"$BRANCH_TMP_FILE"
+
+  # spawn editor only if there are branches to prune
+  if [ -s "$BRANCH_TMP_FILE" ]; then
+    ${EDITOR:-nvim} "$BRANCH_TMP_FILE"
+    xargs git branch -D <"$BRANCH_TMP_FILE"
+  else
+    echo "no branches to purge"
+  fi
 }
