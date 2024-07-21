3f515194a4b615fdbae8d5ab3193e498a64372c1
guppy0130 <guppy0130@yahoo.com>
2024-07-21 15:39:06 -0500

    homebrew path munging

---
+++ zsh/.zsh/applications/clang.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+# vim:tw=2:sw=2
+
+pathmunge "/opt/homebrew/opt/llvm/bin"
---
+++ zsh/.zsh/applications/util-linux.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+pathmunge "/opt/homebrew/opt/util-linux/bin"
+pathmunge "/opt/homebrew/opt/util-linux/sbin"
