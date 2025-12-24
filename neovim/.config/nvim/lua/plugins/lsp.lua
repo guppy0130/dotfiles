d3aa00f6b6ad6535d2888a9872a388d7f2a9ecaf
guppy0130 <guppy0130@yahoo.com>
2025-12-24 14:54:28 -0800

    updates and misc nonsense

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -94,13 +94,13 @@ return {
 				vim.lsp.protocol.make_client_capabilities(),
 				require('cmp_nvim_lsp').default_capabilities()
 			)
-			mason_lspconfig.setup_handlers {
-				function (server_name) -- default handler (optional)
-					lspconfig[server_name].setup {
-						capabilities = capabilities
-					}
-				end
-			}
+			-- mason_lspconfig.setup_handlers {
+			-- 	function (server_name) -- default handler (optional)
+			-- 		lspconfig[server_name].setup {
+			-- 			capabilities = capabilities
+			-- 		}
+			-- 	end
+			-- }
 		end
 	},
 }
